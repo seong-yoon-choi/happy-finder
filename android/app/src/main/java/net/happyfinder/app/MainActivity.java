9fc0192f1fb4829ebc570e956b584603ff0c30fc
choi <sychoi04180605@gmail.com>
2026-03-29 20:19:04 +0900

    fix native google sign in activity wiring

--- android/app/src/main/java/net/happyfinder/app/MainActivity.java
+++ android/app/src/main/java/net/happyfinder/app/MainActivity.java
@@ -2,12 +2,18 @@ package net.happyfinder.app;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import ee.forgr.capacitor.social.login.ModifiedMainActivityForSocialLoginPlugin;
 
-public class MainActivity extends BridgeActivity {
+public class MainActivity extends BridgeActivity implements ModifiedMainActivityForSocialLoginPlugin {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         registerPlugin(AppNotificationSettingsPlugin.class);
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void IHaveModifiedTheMainActivityForTheUseWithSocialLoginPlugin() {
+        // Marker interface required by the Social Login plugin when requesting scopes.
     }
 }
